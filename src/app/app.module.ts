be9a23bf45f2c7377ef283ff809eb2ac5fa2f685
Wataru.Kasahara <kasahara1019@gmail.com>
2020-04-14 09:35:25 +0900

    posts を取得するための repository 実装

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './views/posts/posts.component';
 
 @NgModule({
   declarations: [AppComponent, PostsComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/infrastructures/repositories/post.repository.ts
+++ src/app/infrastructures/repositories/post.repository.ts
@@ -1,8 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../../domain/models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostRepository {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  fetchAll(): Observable<Post[]> {
+    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+  }
 }
