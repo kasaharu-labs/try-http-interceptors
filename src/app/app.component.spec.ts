d3ee219df56b93d2a243a7fc036dc30197033d81
Wataru.Kasahara <kasahara1019@gmail.com>
2020-04-14 09:08:44 +0900

    grep -lr 'ng-basis' ./* | xargs sed -i '' -e 's/ng-basis/try-http-interceptors/g'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-basis'`, () => {
+  it(`should have as title 'try-http-interceptors'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-basis');
+    expect(app.title).toEqual('try-http-interceptors');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-basis app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('try-http-interceptors app is running!');
   });
 });
