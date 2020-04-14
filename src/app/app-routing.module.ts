da176d99ff426137340755e1a82c2e69d95f2980
Wataru.Kasahara <kasahara1019@gmail.com>
2020-04-14 09:19:00 +0900

    PostsComponent を表示

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './views/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: PostsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
